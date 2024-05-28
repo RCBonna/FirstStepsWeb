36bdb13fa767a21c58af599c5821d330ebd2349b
Roberto C Bonanomi <rcbonanomi@gmail.com>
2024-05-28 14:11:21 +0000

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,27 @@
+
+import React from 'react';
+
+const styles = {
+    container: {
+      backgroundColor: '#e6f7ff',
+      height: '100vh',
+      display: 'flex', // Added flexbox for vertical centering
+      justifyContent: 'center', // Center content vertically
+      alignItems: 'center', // Center content horizontally
+    },
+    title: {
+      textAlign: 'center',
+    },
+  };
+
+  
 function Home () {
-    return <h1>RCB - Um novo + velho mundo da WEB.</h1>
+    return (
+        <div className={styles.container}>
+          <title>Primeiros Passos na Web</title>
+          <h1 className={styles.title}>RCB - Um novo + velho mundo da WEB.</h1>
+        </div>
+      );
 }
+
 export default Home;
